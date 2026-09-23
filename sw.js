528156dbd608e91369ecf000953846604d6bb67b
PAVEL <108974875+pkhlystunov@users.noreply.github.com>
2026-09-23 14:46:25 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'otipb-v17';
+const CACHE_NAME = 'otipb-v18';
 const ASSETS = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })))); });
